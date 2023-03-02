34ce20a1841b8a7e7fd0a837e2cb2eed8cf7e876
André Sousa <andresousa808@gmail.com>
2023-03-02 19:20:47 -0300

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily: {
+            'inter': ["Inter"]
+        },
+        maxWidth: {
+            'prose': '100ch'
+        },
+        /* gridTemplateRows: {
+            'layout': ''
+        } */
+    },
+    colors: {
+        'orange-200': 'hsl(35, 77%, 62%)',
+        'red-200': 'hsl(5, 85%, 63%)',
+        'white-100': 'hsl(36, 100%, 99%)',
+        'grayish-blue-400': 'hsl(233, 8%, 79%)',
+        'grayish-blue-600': 'hsl(236, 13%, 42%)',
+        'blue-800': 'hsl(240, 100%, 5%)'
+    },
+    letterSpacing: {
+        'widest': '0.25em'
+    }
+  },
+  plugins: [],
+}
